b200fb3f9e742d06ad286dfbe13cde9be5bc89aa
Jorge <Jorge@gmail>
2018-06-01 20:51:33 -0400

    Cua

--- www/build/0.js
+++ www/build/0.js
@@ -72,7 +72,7 @@ var SeleccionadoPage = /** @class */ (function () {
     };
     SeleccionadoPage = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
-            selector: 'page-seleccionado',template:/*ion-inline-start:"C:\Users\RLCXX\documents\epe1\EPE_1\src\pages\seleccionado\seleccionado.html"*/'<!--\n\n  Generated template for the SeleccionadoPage page.\n\n\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n\n  Ionic pages and navigation.\n\n-->\n\n<ion-header>\n\n\n\n  <ion-navbar>\n\n    <ion-title>seleccionado</ion-title>\n\n  </ion-navbar>\n\n\n\n</ion-header>\n\n\n\n\n\n<ion-content padding>\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\RLCXX\documents\epe1\EPE_1\src\pages\seleccionado\seleccionado.html"*/,
+            selector: 'page-seleccionado',template:/*ion-inline-start:"C:\Users\Ricardo\Desktop\GIT\EPE_1\src\pages\seleccionado\seleccionado.html"*/'<!--\n\n  Generated template for the SeleccionadoPage page.\n\n\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n\n  Ionic pages and navigation.\n\n-->\n\n<ion-header>\n\n\n\n  <ion-navbar>\n\n    <ion-title>seleccionado</ion-title>\n\n  </ion-navbar>\n\n\n\n</ion-header>\n\n\n\n\n\n<ion-content padding>\n\n\n\n</ion-content>\n\n'/*ion-inline-end:"C:\Users\Ricardo\Desktop\GIT\EPE_1\src\pages\seleccionado\seleccionado.html"*/,
         }),
         __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavParams */]])
     ], SeleccionadoPage);
